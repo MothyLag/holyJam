ae267ae28e4cac089549b582faa0ac64f335be81
Eduardo Monter <eduardo.monter@pcsynergy.com>
2023-11-30 18:16:36 -0600

    deploying changes

---
+++ src/components/container/container.tsx
@@ -0,0 +1,9 @@
+const Container = () => {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen full no-scrollbar">
+      <h1>foto</h1>
+    </div>
+  );
+};
+
+export default Container;
