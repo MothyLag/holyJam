114214bf3ee7ec6d78b418a1ae65ac68fdbe1b89
Eduardo Monter <eduardo.monter@pcsynergy.com>
2023-12-01 01:50:49 -0600

    adding form styles

--- src/components/SubmitForm/SubmitForm.tsx
+++ src/components/SubmitForm/SubmitForm.tsx
@@ -1,15 +1,17 @@
 const SubmitForm = () => {
   return (
     <div className="h-screen w-screen flex justify-around items-center flex-col">
-      <div className="h-[10%] w-[100%] flex items-center flex-col">
-        <label>Ingresa tu nombre: </label>
-        <input className="w-[50%]" />
+      <div className="h-[10%] w-[50%] flex items-center flex-col">
+        <label className="flex self-start">Ingresa tu nombre: </label>
+        <input className="w-[100%] outlined focus:outlined" />
       </div>
-      <div className="h-[10%] w-[100%] flex items-center flex-col">
-        <input className="w-[50%]" />
+      <div className="h-[10%] w-[50%] flex items-center flex-col">
+        <label className="flex self-start">Ingresa una frase: </label>
+        <input className="w-[100%] outlined focus:outlined" />
       </div>
-      <div className="h-[10%] w-[100%] flex items-center flex-col">
-        <input className="w-[50%]" />
+      <div className="h-[10%] w-[50%] flex items-center flex-col">
+        <label className="flex self-start">Ingresa una restricción: </label>
+        <input className="w-[100%] outlined focus:outlined" />
       </div>
     </div>
   );
