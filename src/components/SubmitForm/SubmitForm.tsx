faf9a4d7b6449f29d2c25249ad9d17e91237730c
AxelRUI <66336304+AxelRUI@users.noreply.github.com>
2023-12-01 01:57:50 -0600

    css

--- src/components/SubmitForm/SubmitForm.tsx
+++ src/components/SubmitForm/SubmitForm.tsx
@@ -3,15 +3,15 @@ const SubmitForm = () => {
     <div className="h-screen w-screen flex justify-around items-center flex-col">
       <div className="h-[10%] w-[50%] flex items-center flex-col">
         <label className="flex self-start">Ingresa tu nombre: </label>
-        <input className="w-[100%] outlined focus:outlined" />
+        <input className="w-[100%]" />
       </div>
       <div className="h-[10%] w-[50%] flex items-center flex-col">
         <label className="flex self-start">Ingresa una frase: </label>
-        <input className="w-[100%] outlined focus:outlined" />
+        <input className="w-[100%]" />
       </div>
       <div className="h-[10%] w-[50%] flex items-center flex-col">
         <label className="flex self-start">Ingresa una restricción: </label>
-        <input className="w-[100%] outlined focus:outlined" />
+        <input className="w-[100%]" />
       </div>
     </div>
   );
